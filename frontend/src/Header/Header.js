56e09ea8576f8d2a333dbe30a93242d2b11961ab
Kati Fanomezantsoa <kati@northstreetcreative.com>
2024-10-30 16:27:58 +0300

    project set up

---
+++ frontend/src/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Products</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
